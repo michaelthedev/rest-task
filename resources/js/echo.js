d1aaf0c2543c8af443d31fd25f5d878199a3f7ee
Michael A. <39175160+michaelthedev@users.noreply.github.com>
2024-05-19 03:53:53 +0100

    chore: web view for websocket test

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -7,8 +7,30 @@ window.Echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
     wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+    wsPort: import.meta.env.VITE_REVERB_PORT,
+    wssPort: import.meta.env.VITE_REVERB_PORT,
     forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
     enabledTransports: ['ws', 'wss'],
+    auth: {
+        headers: {
+            Authorization: 'Bearer JWT_TOKEN_HERE'
+        }
+    }
 });
+
+setTimeout(() => {
+    window.Echo.private('App.Models.User.1')
+        .listen('TaskAdded', (e) => {
+            console.log("TaskAdded event received")
+            console.log(e)
+        })
+        .listen('TaskUpdated', (e) => {
+            console.log("TaskUpdated event received")
+            console.log(e)
+        })
+        .listen('TaskDeleted', (e) => {
+            console.log("TaskUpdated event received")
+            console.log(e)
+        })
+}, 2000);
+
